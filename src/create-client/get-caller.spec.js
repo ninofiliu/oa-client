2942003999e5c547bc381fe5b43cc40a4796ffaa
ninofiliu <nino.filiu@gmail.com>
2020-04-24 20:25:19 +0200

    add src/create-client/get-caller

---
+++ src/create-client/get-caller.spec.js
@@ -0,0 +1,22 @@
+import getCaller from './get-caller';
+
+describe('getCaller', () => {
+  it('throws if the route specs does not specify a type', () => {
+    const callers = {};
+    const routeSpecs = {};
+
+    expect(() => getCaller(callers, routeSpecs, '/a')).toThrow();
+  });
+  it('throws if the route specs specifies a type that is not a caller', () => {
+    const callers = {};
+    const routeSpecs = { type: 'foo' };
+
+    expect(() => getCaller(callers, routeSpecs, '/a')).toThrow();
+  });
+  it('returns callers[routeSpecs.type]', () => {
+    const callers = { foo: 'bar' };
+    const routeSpecs = { type: 'foo' };
+
+    expect(getCaller(callers, routeSpecs, '/a')).toEqual('bar');
+  });
+});
