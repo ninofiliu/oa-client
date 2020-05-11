02c0ab13f9e89285af3524df06bd7c174dd9dd58
ninofiliu <nino.filiu@gmail.com>
2020-05-11 14:04:36 +0200

    migrate to key-based API

--- src/create-client/index.spec.js
+++ src/create-client/index.spec.js
@@ -1,16 +1,32 @@
 import createClient from '.';
 
 describe('createClient', () => {
-  it('has one method for each HTTP method', () => {
+  it('should call the right caller with a compiled URL and the body', () => {
     const specs = {
-      servers: [{ url: 'http://my.server.com' }],
-      paths: {},
+      servers: [
+        { url: 'http://my.server.com' },
+      ],
+      paths: {
+        '/users/{id}': {
+          post: {
+            'x-type': 'simple-post',
+          },
+        },
+      },
     };
-    const callers = {};
-    const client = createClient(specs, callers);
+    const callers = {
+      'simple-post': jest.fn(),
+    };
 
-    for (const method of ['get', 'head', 'post', 'put', 'delete', 'options', 'trace', 'patch']) {
-      expect(typeof client[method]).toEqual('function');
-    }
+    const client = createClient(specs, callers, { validationLevel: 'error' });
+    client['/users/{id}'].post({
+      pathParams: { id: '123456' },
+      queryParams: { foo: 'bar' },
+      body: { some: 'data' },
+    });
+
+    const url = 'http://my.server.com/users/123456?foo=bar';
+    expect(callers['simple-post'].mock.calls[0][0].toString()).toEqual(url);
+    expect(callers['simple-post'].mock.calls[0][1]).toEqual({ some: 'data' });
   });
 });
--- src/create-client/index.ts
+++ src/create-client/index.ts
@@ -1,5 +1,9 @@
-import createClientMethod from './create-client-method';
-import { Callers, Specs, ValidationLevel } from '../types';
+import getCaller from './get-caller';
+import getOrigin from './get-origin';
+import getCallPath from './get-call-path';
+import validateRequest from './validate-request';
+import { Specs, Callers, ValidationLevel, Params } from '../types';
+import OAClientError from '../errors/OAClientError';
 
 export default (
   specs: Specs,
@@ -10,13 +14,38 @@ export default (
   }
   : { origin: null | string, validationLevel: ValidationLevel }
   = { origin: null, validationLevel: 'off' },
-) => ({
-  get: createClientMethod(specs, callers, 'get', origin, validationLevel),
-  head: createClientMethod(specs, callers, 'head', origin, validationLevel),
-  post: createClientMethod(specs, callers, 'post', origin, validationLevel),
-  put: createClientMethod(specs, callers, 'put', origin, validationLevel),
-  delete: createClientMethod(specs, callers, 'delete', origin, validationLevel),
-  options: createClientMethod(specs, callers, 'options', origin, validationLevel),
-  trace: createClientMethod(specs, callers, 'trace', origin, validationLevel),
-  patch: createClientMethod(specs, callers, 'patch', origin, validationLevel),
+) => new Proxy({}, {
+  get(client, path: string) {
+    if (!(path in specs.paths)) {
+      throw new OAClientError(4, { path });
+    }
+    return new Proxy({}, {
+      get(clientPath, method) {
+        if (!(method in specs.paths[path])) {
+          throw new OAClientError(5, { path, method, specs });
+        }
+        return (
+          {
+            pathParams = {},
+            queryParams = {},
+            body = null,
+            contentType = 'application/json',
+          }
+          : { pathParams: Params, queryParams: Params, body: any, contentType: string }
+          = { pathParams: {}, queryParams: {}, body: null, contentType: 'application/json' },
+        ) => {
+          const routeSpecs = specs.paths[path][method];
+          validateRequest(validationLevel, routeSpecs, pathParams, queryParams, body, contentType);
+          const caller = getCaller(callers, routeSpecs, path);
+          const callOrigin = getOrigin(origin, specs);
+          const callPath = getCallPath(path, pathParams);
+          const callUrl = new URL(callOrigin + callPath);
+          for (const key in queryParams) {
+            callUrl.searchParams.append(key, queryParams[key]);
+          }
+          return caller(callUrl, body);
+        };
+      },
+    });
+  },
 });
