356cb65713e3c4a3ef0167a0a36e756215dc7cd5
ninofiliu <nino.filiu@gmail.com>
2020-05-11 14:19:48 +0200

    add tests

--- src/create-client/index.spec.js
+++ src/create-client/index.spec.js
@@ -1,24 +1,41 @@
 import createClient from '.';
 
-describe('createClient', () => {
-  it('should call the right caller with a compiled URL and the body', () => {
-    const specs = {
-      servers: [
-        { url: 'http://my.server.com' },
-      ],
-      paths: {
-        '/users/{id}': {
-          post: {
-            'x-type': 'simple-post',
-          },
-        },
+const specs = {
+  servers: [
+    { url: 'http://my.server.com' },
+  ],
+  paths: {
+    '/users/{id}': {
+      post: {
+        'x-type': 'simple-post',
       },
-    };
-    const callers = {
+    },
+  },
+};
+let callers;
+let client;
+
+describe('createClient', () => {
+  beforeEach(() => {
+    callers = {
       'simple-post': jest.fn(),
     };
-
-    const client = createClient(specs, callers, { validationLevel: 'error' });
+    client = createClient(specs, callers, { validationLevel: 'error' });
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('throws explicitly for non-existing path', () => {
+    expect(() => {
+      client['/non/existing/path'].get();
+    }).toThrow('[oa-client:4]');
+  });
+  it('throws explicitly for non-existing methods', () => {
+    expect(() => {
+      client['/users/{id}'].get();
+    }).toThrow('[oa-client:5]');
+  });
+  it('should call the right caller with a compiled URL and the body', () => {
     client['/users/{id}'].post({
       pathParams: { id: '123456' },
       queryParams: { foo: 'bar' },
