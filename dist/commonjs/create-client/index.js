428c2a1ed9b382ee2a44dac9eb87362611a9e564
ninofiliu <nino.filiu@gmail.com>
2020-05-11 14:05:50 +0200

    build

--- dist/commonjs/create-client/index.js
+++ dist/commonjs/create-client/index.js
@@ -3,14 +3,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const create_client_method_1 = __importDefault(require("./create-client-method"));
-exports.default = (specs, callers, { origin = null, validationLevel = 'off', } = { origin: null, validationLevel: 'off' }) => ({
-    get: create_client_method_1.default(specs, callers, 'get', origin, validationLevel),
-    head: create_client_method_1.default(specs, callers, 'head', origin, validationLevel),
-    post: create_client_method_1.default(specs, callers, 'post', origin, validationLevel),
-    put: create_client_method_1.default(specs, callers, 'put', origin, validationLevel),
-    delete: create_client_method_1.default(specs, callers, 'delete', origin, validationLevel),
-    options: create_client_method_1.default(specs, callers, 'options', origin, validationLevel),
-    trace: create_client_method_1.default(specs, callers, 'trace', origin, validationLevel),
-    patch: create_client_method_1.default(specs, callers, 'patch', origin, validationLevel),
+const get_caller_1 = __importDefault(require("./get-caller"));
+const get_origin_1 = __importDefault(require("./get-origin"));
+const get_call_path_1 = __importDefault(require("./get-call-path"));
+const validate_request_1 = __importDefault(require("./validate-request"));
+const OAClientError_1 = __importDefault(require("../errors/OAClientError"));
+exports.default = (specs, callers, { origin = null, validationLevel = 'off', } = { origin: null, validationLevel: 'off' }) => new Proxy({}, {
+    get(client, path) {
+        if (!(path in specs.paths)) {
+            throw new OAClientError_1.default(4, { path });
+        }
+        return new Proxy({}, {
+            get(clientPath, method) {
+                if (!(method in specs.paths[path])) {
+                    throw new OAClientError_1.default(5, { path, method, specs });
+                }
+                return ({ pathParams = {}, queryParams = {}, body = null, contentType = 'application/json', } = { pathParams: {}, queryParams: {}, body: null, contentType: 'application/json' }) => {
+                    const routeSpecs = specs.paths[path][method];
+                    validate_request_1.default(validationLevel, routeSpecs, pathParams, queryParams, body, contentType);
+                    const caller = get_caller_1.default(callers, routeSpecs, path);
+                    const callOrigin = get_origin_1.default(origin, specs);
+                    const callPath = get_call_path_1.default(path, pathParams);
+                    const callUrl = new URL(callOrigin + callPath);
+                    for (const key in queryParams) {
+                        callUrl.searchParams.append(key, queryParams[key]);
+                    }
+                    return caller(callUrl, body);
+                };
+            },
+        });
+    },
 });
--- dist/es2015/create-client/index.js
+++ dist/es2015/create-client/index.js
@@ -1,11 +1,31 @@
-import createClientMethod from './create-client-method';
-export default (specs, callers, { origin = null, validationLevel = 'off', } = { origin: null, validationLevel: 'off' }) => ({
-    get: createClientMethod(specs, callers, 'get', origin, validationLevel),
-    head: createClientMethod(specs, callers, 'head', origin, validationLevel),
-    post: createClientMethod(specs, callers, 'post', origin, validationLevel),
-    put: createClientMethod(specs, callers, 'put', origin, validationLevel),
-    delete: createClientMethod(specs, callers, 'delete', origin, validationLevel),
-    options: createClientMethod(specs, callers, 'options', origin, validationLevel),
-    trace: createClientMethod(specs, callers, 'trace', origin, validationLevel),
-    patch: createClientMethod(specs, callers, 'patch', origin, validationLevel),
+import getCaller from './get-caller';
+import getOrigin from './get-origin';
+import getCallPath from './get-call-path';
+import validateRequest from './validate-request';
+import OAClientError from '../errors/OAClientError';
+export default (specs, callers, { origin = null, validationLevel = 'off', } = { origin: null, validationLevel: 'off' }) => new Proxy({}, {
+    get(client, path) {
+        if (!(path in specs.paths)) {
+            throw new OAClientError(4, { path });
+        }
+        return new Proxy({}, {
+            get(clientPath, method) {
+                if (!(method in specs.paths[path])) {
+                    throw new OAClientError(5, { path, method, specs });
+                }
+                return ({ pathParams = {}, queryParams = {}, body = null, contentType = 'application/json', } = { pathParams: {}, queryParams: {}, body: null, contentType: 'application/json' }) => {
+                    const routeSpecs = specs.paths[path][method];
+                    validateRequest(validationLevel, routeSpecs, pathParams, queryParams, body, contentType);
+                    const caller = getCaller(callers, routeSpecs, path);
+                    const callOrigin = getOrigin(origin, specs);
+                    const callPath = getCallPath(path, pathParams);
+                    const callUrl = new URL(callOrigin + callPath);
+                    for (const key in queryParams) {
+                        callUrl.searchParams.append(key, queryParams[key]);
+                    }
+                    return caller(callUrl, body);
+                };
+            },
+        });
+    },
 });
