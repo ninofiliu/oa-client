459cd034bc3037362411eac173e4ff9c18a01a6d
Nino Filiu <nino.filiu@gmail.com>
2022-01-17 11:29:43 +0100

    remove extraneous path param and rewrite tests

--- src/create-client/get-caller.spec.js
+++ src/create-client/get-caller.spec.js
@@ -1,29 +1,22 @@
 import getCaller from './get-caller';
 
 describe('getCaller', () => {
-  it('throws if the caller does not provide a fallback default function for get', () => {
-    const callers = {};
-    const routeSpecs = {};
-
-    expect(() => getCaller(callers, routeSpecs, '/a', 'get')).toThrow('[oa-client:2]');
-  });
-  
-  it('throws if the route specs specifies a type that is not a caller', () => {
-    const callers = {};
+  it('returns callers[routeSpecs[\'x-type\']] if it exists', () => {
+    const callers = { foo: 'FOO_CALLER' };
     const routeSpecs = { 'x-type': 'foo' };
 
-    expect(() => getCaller(callers, routeSpecs, '/a', 'get')).toThrow('[oa-client:2]');
+    expect(getCaller(callers, routeSpecs, 'get')).toEqual('FOO_CALLER');
   });
-  it('returns callers[get]', () => {
-    const callers = { get : 'bar' };
+  it('returns callers[method] if it exists', () => {
+    const callers = { get: 'GET_CALLER', post: 'POST_CALLER' };
     const routeSpecs = { };
-    
-    expect(getCaller(callers, routeSpecs, '/a', 'get')).toEqual('bar');
+
+    expect(getCaller(callers, routeSpecs, 'post')).toEqual('POST_CALLER');
   });
-  it('returns callers[routeSpecs[\'x-type\']]', () => {
-    const callers = { foo: 'bar' };
+  it('throws if no caller matches', () => {
+    const callers = {};
     const routeSpecs = { 'x-type': 'foo' };
 
-    expect(getCaller(callers, routeSpecs, '/a', 'get')).toEqual('bar');
+    expect(() => getCaller(callers, routeSpecs, 'get')).toThrow('[oa-client:2]');
   });
 });
--- src/create-client/get-caller.ts
+++ src/create-client/get-caller.ts
@@ -1,12 +1,8 @@
 import OAClientError from '../errors/OAClientError';
 import { Callers, RouteSpecs } from '../types';
 
-export default (callers: Callers, routeSpecs: RouteSpecs, path: string, method :string) => {
-  // Fallback to a default call for the specified method
+export default (callers: Callers, routeSpecs: RouteSpecs, method :string) => {
   const type = routeSpecs['x-type'] ?? method;
-
-  if (!(type in callers)) {
-    throw new OAClientError(2, { type, callers });
-  }
+  if (!(type in callers)) throw new OAClientError(2, { type, callers });
   return callers[type];
 };
