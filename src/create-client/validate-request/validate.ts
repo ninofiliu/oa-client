9245ce8bee35f27bb63918328d6986a825d3b1b0
Nino Filiu <nino.filiu@gmail.com>
2022-08-09 00:39:36 +0200

    #12: add support for formats

--- src/create-client/validate-request/validate.ts
+++ src/create-client/validate-request/validate.ts
@@ -1,7 +1,9 @@
 import Ajv from "ajv";
 import OAClientError from "../../errors/OAClientError";
+import addFormats from "ajv-formats";
 
 const ajv = new Ajv();
+addFormats(ajv);
 
 export default (schema: any, data: any) => {
   if (!schema) throw new OAClientError(105, { schema });
